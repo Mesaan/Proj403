516e1a967d39d59dd154c72e7feb30ec78f042eb
cath0007 <jeanne.cathe@etudiant.univ-reims.fr>
2023-03-02 10:30:14 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[]){
+//Noter choix création récursive OU proc principal q fait tout.
+    exit(0); 
+}
